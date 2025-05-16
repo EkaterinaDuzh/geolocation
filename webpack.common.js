dd6a78f0729b73f34d86d6b8e75c705701da18fd
Ekaterina <ekaterinaduzh@gmail.com>
2025-05-16 16:13:25 +0300

    Убрать лишние символы

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  target: 'web',
+  target: "web",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/[name][ext]',
-    publicPath: '',
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[name][ext]",
+    publicPath: "",
   },
   module: {
     rules: [
@@ -15,53 +15,56 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
+            loader: "html-loader",
           },
         ],
       },
       {
         test: /\.css$/,
         use: [
-          MiniCssExtractPlugin.loader, {
-            loader: 'css-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
             options: {
               esModule: false,
-            }
-          }
+            },
+          },
         ],
       },
       {
         test: /\.svg$/,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(png|jpg|gif|ico)$/i,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            esModule: false,
-            name: '[name].[ext]',
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name: "[name].[ext]",
+            },
           },
-        }],
+        ],
       },
     ],
   },
 
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
+      template: "./src/index.html",
+      filename: "./index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
+      filename: "[name].css",
+      chunkFilename: "[id].css",
     }),
   ],
 };
